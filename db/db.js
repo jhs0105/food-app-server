a9d4cdbfb77b88993a1750a1ec66d7cc9de34151
jhs0105 <114391420+jhs0105@users.noreply.github.com>
2024-04-24 23:20:04 +0900

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const db = mongoose
   .connect(
-    `mongodb+srv://fpaldps:${process.env.MONGO_PASSWORD}@cluster0.g1ce30s.mongodb.net/?retryWrites=true&w=majority`,
+    `mongodb+srv://fpaldps:${process.env.MONGO_PASSWORD}@cluster0.g1ce30s.mongodb.net/`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
