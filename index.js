b9b49e8dd63257debfef3b2c3dd9cfdb7a40ba76
jhs0105 <fpaldps@naver.com>
2024-05-19 23:07:23 +0900

    port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/public")));
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 3000);
 
 const PORT = app.get("port");
 
